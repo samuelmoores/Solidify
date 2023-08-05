03111c1dd45a17614cdc2c574d55d047cdec6f1e
samuelmoores <samueldmoores@gmail.com>
2023-08-05 10:22:00 -0700

    Zack pull test #2

--- SolidifyProject/Assets/Scripts/HelloWorld.cs
+++ SolidifyProject/Assets/Scripts/HelloWorld.cs
@@ -7,7 +7,7 @@ public class HelloWorld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World Sam's script weeeeeeeeee :DDDDD");
+        Debug.Log("Hello World Sam's script weeeeeeeeee :DDDDD, pull tes number 2");
     }
 
     // Update is called once per frame
