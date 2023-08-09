b5ad13c9d72f34e8f299fe97ad556dbeabd4ca9b
samuelmoores <samueldmoores@gmail.com>
2023-08-09 12:19:39 -0700

    Yeti throws snowballs at player

--- SolidifyProject/Assets/Scripts/EnemyController.cs
+++ SolidifyProject/Assets/Scripts/EnemyController.cs
@@ -5,20 +5,91 @@ using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
-    public GameObject PatrolLoc;
+    public GameObject Destination;
+    public GameObject SnowBallRef;
+    public Transform ThrowRef;
+    public Camera cam;
+
     NavMeshAgent agent;
+    Animator animator;
+    float attackCooldown;
+    bool isThrowing;
 
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
         agent.speed = 1.0f;
+        agent.isStopped = true;
+        animator.SetBool("canThrow", false);
+        animator.SetBool("isWalking", false);
+        attackCooldown = 5.0f;
+        isThrowing = false;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.destination = PatrolLoc.transform.position;
+        agent.destination = Destination.transform.position;
+
+        if (agent.remainingDistance < 20 && !agent.pathPending)
+        {
+            attackCooldown -= Time.deltaTime;
+            Debug.Log(attackCooldown);
+
+            //Stop the yeti and play throw animation
+            agent.isStopped = true;
+            animator.SetBool("isWalking", false);
+
+            //Make sure the yeti faces the player
+            agent.updateRotation = true;
+            Vector3 aim = transform.position - agent.destination;
+            Quaternion aimRotation = Quaternion.LookRotation(aim);
+            aimRotation *= Quaternion.Euler(0, 180, 0);
+            transform.rotation = aimRotation;
+
+            //Start animation
+            animator.SetBool("canThrow", true);
+
+            //Throw snow ball
+            if (attackCooldown < 4.25f && !isThrowing)
+                ThrowSnowBall();
+
+            //Transition out of animation
+            if(attackCooldown < 3 && attackCooldown > 0)
+            {
+                animator.SetBool("canThrow", false);
+            }
+
+            //Wait for next throw
+            if (attackCooldown < 0)
+            {
+                animator.SetBool("canThrow", true);
+                attackCooldown = 5;
+                isThrowing = false;
+            }
+
+        }
+        else if(agent.remainingDistance > 20 && !agent.pathPending)
+        {
+            agent.isStopped = false;
+            animator.SetBool("isWalking", true);
+            animator.SetBool("canThrow", false);
+
+        }
+
+    }
+
+    void ThrowSnowBall()
+    {
+
+        //Throw Snow Ball
+        GameObject SnowBall = Instantiate(SnowBallRef, ThrowRef.position, Quaternion.identity);
+        SnowBall.GetComponent<Rigidbody>().AddForce(ThrowRef.transform.forward * 450, ForceMode.Impulse);
+        isThrowing = true;
+       
 
     }
 }
